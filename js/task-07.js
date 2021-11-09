d73a0d36c55bce66d5f2e8f936b77c63bd697b32
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-11-09 21:59:17 +0200

    Finally redone
    task #7

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,13 @@
+
 const controlFontEl = document.querySelector("#font-size-control");
 
 const textEl = document.querySelector("#text");
 
-// controlFontEl.oninput = function() {
-//   textEl.style.fontSize = controlFontEl.value + 'px';
-// };
-
 
 controlFontEl.addEventListener("input", handleInputRange);
-function handleInputRange(event) {
-  textEl.style.fontSize = event.currentTarget.value + "px";
+textEl.style.fontSize = controlFontEl.value + 'px';
+
+function handleInputRange() {
+  
+  textEl.style.fontSize = controlFontEl.value + "px";
 }
