6daedc7f66811dd1368aea7b7b012e8d4a201785
Lelyak-Andriy <a.lelyak47@gmail.com>
2021-11-09 20:51:21 +0200

    redone 6

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,12 @@ const controlFontEl = document.querySelector("#font-size-control");
 
 const textEl = document.querySelector("#text");
 
-controlFontEl.oninput = function() {
-  textEl.style.fontSize = controlFontEl.value + 'px';
-};
+// controlFontEl.oninput = function() {
+//   textEl.style.fontSize = controlFontEl.value + 'px';
+// };
+
+
+controlFontEl.addEventListener("input", handleInputRange);
+function handleInputRange(event) {
+  textEl.style.fontSize = event.currentTarget.value + "px";
+}
